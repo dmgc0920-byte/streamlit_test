99c57f505184ac9787f429125656e19261d2368e
dmgc0920-byte <dmgc0920@gmail.com>
2025-10-02 11:29:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+<<<<<<< Updated upstream
 import pandas as pd
+=======
+>>>>>>> Stashed changes
 import streamlit as st
     
 st.header('Lanzar una moneda')
 
 st.write('Esta aplicación aún no es funcional. En construcción.')
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
